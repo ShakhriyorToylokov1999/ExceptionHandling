b1124b6b21cec1b4509bd1fc1aefdfb67b1667d8
ShakhriyorToylokov1999 <118186892+ShakhriyorToylokov1999@users.noreply.github.com>
2023-05-26 20:02:13 +0500

    Add project files.

---
+++ Task1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+
+namespace Task1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            // TODO: Implement the task here.
+            string inputText="";
+            bool stop = false;
+            
+            Console.WriteLine($"Text first letter checker \n " +
+                              $"Enter 0 when you want to stop entering texts: ");
+            do
+            {
+                Console.Write("Enter text: ");
+                if (inputText!="0")
+                {
+                    try
+                    {
+                        inputText = Console.ReadLine();
+                        Console.WriteLine("The first char is : " + inputText[0]);
+                    }
+                    catch (IndexOutOfRangeException ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+                else
+                {
+                    stop= true;
+                }
+            } while (!stop);
+
+
+        }
+    }
+}
